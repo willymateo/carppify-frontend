cb5c968fda6f7a28e2085f07abc8df0afbaa60e4
willymateo <wjmateo@espol.edu.ec>
2022-09-07 10:56:52 -0500

    add welcome message

--- pages/index.js
+++ pages/index.js
@@ -22,35 +22,42 @@ export default function Home() {
   return (
     <Stack direction="column" justifyContent="space-between" flex={1} margin={2}>
       <Header />
-
       <Box marginY={5}>
-        <Container>
-          <Typography variant="h4" textAlign="center">
-            {driver_id ? `Vehicles of driver with ID: ${driver_id}` : "Vehicles by Driver ID"}
-          </Typography>
-        </Container>
+        {driver_id ? (
+          <>
+            <Container>
+              <Typography variant="h4" textAlign="center">
+                {`Vehicles of driver with ID: ${driver_id}`}
+              </Typography>
+            </Container>
 
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell align="center">ID</TableCell>
-                <TableCell align="center">Plate</TableCell>
-                <TableCell align="center">Model</TableCell>
-                <TableCell align="center">Type</TableCell>
-                <TableCell align="center">Capacity</TableCell>
-                <TableCell align="center">Creation Date</TableCell>
-                <TableCell align="center">Edit</TableCell>
-                <TableCell align="center">Delete</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {vehicles.map(vehicle => (
-                <VehicleTableRow key={vehicle.id} {...vehicle} />
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="center">ID</TableCell>
+                    <TableCell align="center">Plate</TableCell>
+                    <TableCell align="center">Model</TableCell>
+                    <TableCell align="center">Type</TableCell>
+                    <TableCell align="center">Capacity</TableCell>
+                    <TableCell align="center">Creation Date</TableCell>
+                    <TableCell align="center">Edit</TableCell>
+                    <TableCell align="center">Delete</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {vehicles.map(vehicle => (
+                    <VehicleTableRow key={vehicle.id} {...vehicle} />
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </>
+        ) : (
+          <Typography variant="h4" textAlign="center">
+            Welcome, please enter a driver ID
+          </Typography>
+        )}
       </Box>
 
       <Footer />
